7ae148239cec0540dfef8cab3fd1d9549044e0b8
gustavorafaeldev <grlrafael@outlook.com>
2022-10-04 11:37:14 -0300

    página inicial

---
+++ src/main/java/com/dev/Shop/controller/PrincipalController.java
@@ -0,0 +1,14 @@
+package com.dev.Shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+    
+    @GetMapping("/admin")
+    public String acessarPrincipal() {
+        return "admin/home";
+    }
+
+}
